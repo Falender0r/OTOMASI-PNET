c9f5cda501f6ab138d4e5122b452f143f2a89b1a
Falender0r <buatsesuatu12@gmail.com>
2024-12-18 17:36:45 +0700

    miktomasi.sh

--- miktomasi.sh
+++ miktomasi.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# MIKROTIK Configuration
-
+# MIROKTIK
 IPNET="192.168.74.137"
-MIKROTIK_IP="192.168.200.11"   # IP MikroTik lama (disesuaikan dengan absen 11)
-MIKROTIK_S="192.168.200.0"     # Subnet IP MikroTik
-MPORT="30003"                  # Port Telnet
+MIKROTIK_IP="192.168.200.1"  # IP MikroTik yang baru
+MIKROTIK_S="192.168.200.0"
+MPORT="30005"
 
-# Konfigurasi MikroTik via Telnet
 expect << EOF > /dev/null
 spawn telnet $IPNET $MPORT
 expect "Mikrotik Login:"
@@ -15,41 +13,46 @@ send "admin\r"
 expect "Password:"
 send "\r"
 
-# Ubah password
 expect ">"
-send "n\r"
+send "n"
+
 expect "new password"
 send "123\r"
-expect "retype new password"
+
+expect "repeat new password"
 send "123\r"
 
-# Menambahkan VLAN 11 pada ether1
 expect ">"
-send "/interface vlan add name=VLAN11 vlan-id=11 interface=ether1\r"
 
-# Memberikan IP Address pada VLAN11
-expect ">"
-send "/ip address add address=192.168.11.2/24 interface=VLAN11\r"
+# Menambahkan alamat IP pada interface ether2
+send "/ip address add address=192.168.200.1/24 interface=ether2\r"
 
-# Menambahkan default route menuju Ubuntu Server (gateway)
-expect ">"
-send "/ip route add dst-address=0.0.0.0/0 gateway=192.168.11.1\r"
+# Menambahkan DHCP Client pada interface ether1 jika MikroTik terhubung ke internet
+send "/ip dhcp-client add interface=ether1 disabled=no\r"
 
-# Menambahkan NAT untuk akses internet jika diperlukan
-expect ">"
-send "/ip firewall nat add chain=srcnat out-interface=ether2 action=masquerade\r"
+# Menambahkan DHCP Pool untuk distribusi alamat IP pada jaringan 192.168.200.0/24
+send "/ip pool add name=dhcp_pool ranges=192.168.200.2-192.168.200.200\r"
 
-# Mengatur DNS
-expect ">"
-send "/ip dns set servers=8.8.8.8,8.8.4.4\r"
+# Menambahkan DHCP Server untuk interface ether2
+send "/ip dhcp-server add name=dhcp1 interface=ether2 address-pool=dhcp_pool\r"
 
-# Save konfigurasi
-expect ">"
-send "/system reboot\r"
-expect "Do you want to reboot"
-send "y\r"
+# Menambahkan network untuk DHCP Server MikroTik
+send "/ip dhcp-server network add address=192.168.200.0/24 gateway=192.168.200.1 dns-server=8.8.8.8\r"
+
+# Mengaktifkan DHCP Server
+send "/ip dhcp-server enable dhcp1\r"
+
+# Menambahkan NAT (Masquerading) untuk akses internet dari jaringan lokal
+send "/ip firewall nat add chain=srcnat out-interface=ether1 action=masquerade\r"
+
+# Menambahkan route untuk jaringan Ubuntu (Jika Ubuntu berada di jaringan 192.168.11.0/24)
+send "/ip route add dst-address=192.168.11.0/24 gateway=192.168.200.1\r"
+
+# Mengaktifkan IP forwarding pada MikroTik (Penting untuk routing antar jaringan)
+send "/ip settings set ip-forwarding=yes\r"
+
+# Mengaktifkan opsi IP forwarding untuk perangkat yang ada pada jaringan ini
+send "/interface ethernet set ether2 arp=proxy-arp\r"
 
 expect eof
 EOF
-
-echo "Konfigurasi MikroTik selesai via Telnet."
